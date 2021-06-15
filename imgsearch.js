6065a78434cac5c6fa73df96887474e0a860ffd1
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-15 12:22:02 +0530

    Update imgsearch.js

--- imgsearch.js
+++ imgsearch.js
@@ -4,9 +4,9 @@ const got = require("got");
 
 Asena.addCommand({pattern: 'imgs ?(.*)', fromMe: true, desc: "Search any number of pictures" }, (async (message, match) => { 
     if (match[1] === '') return await message.client.sendMessage(message.jid,"What do you what to search : number of images want",MessageType.text);
-    var arg = match[1].split(':')[0] ;
+    var arg = match[1].split(':') ;
 	if (arg[1]=== '') arg[1] = 5 ;
-	gis(match[1], async (error, result) => {
+	gis(arg[0], async (error, result) => {
         for (var i = 0; i < (result.length < arg[1] ? result.length : arg[1]); i++) {
             var get = got(result[i].url, {https: {rejectUnauthorized: false}});
             var stream = get.buffer();
@@ -16,6 +16,6 @@ Asena.addCommand({pattern: 'imgs ?(.*)', fromMe: true, desc: "Search any number 
             });
         }
 
-        message.reply("Uploading photo ```{}``` number ```{} `..."((result.length < arg[1] ? result.length : arg[1]), match[1]));
+        message.reply("Uploading photo ```{}``` number ```{} `..."((result.length < arg[1] ? result.length : arg[1]), arg[0]));
     });
 }));
