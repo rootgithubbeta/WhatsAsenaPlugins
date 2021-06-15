6ce8c4e9a1413e2a0ae38bbe0095b9f6f9aed9cb
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-15 16:36:44 +0530

    Update imgsearch.js

--- imgsearch.js
+++ imgsearch.js
@@ -1,13 +1,15 @@
-const Asena = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const got = require("got");
 
 Asena.addCommand({pattern: 'imgs ?(.*)', fromMe: true, desc: "Search any number of pictures" }, (async (message, match) => { 
-    if (match[1] == '') return await message.client.sendMessage(message.jid,"What do you what to search : number of images want",MessageType.text);
-	parseInt((match[1].split(':')[0]).split(':')[1]) = arg ;
-	if ((match[1].split(':')[0]).split(':')[1] == '') var arg = 5 ;
-	gis((match[1].split(':')[0]).split(':')[0], async (error, result) => {
-        for (var i = 0; i < (result.length < arg ? result.length : arg); i++) {
+    await message.client.sendMessage(message.jid,match[1],MessageType.text);	
+    if (match[1] === '') return await message.client.sendMessage(message.jid,"What do you what to search : number of images want",MessageType.text);
+    var arg = match[1].split(':') ;
+	await message.client.sendMessage(message.jid,arg[0],MessageType.text);
+	await message.client.sendMessage(message.jid,arg[1],MessageType.text);
+	if (arg[1]=== '') arg[1] = 5 ;
+	gis(arg[0], async (error, result) => {
+        for (var i = 0; i < (result.length < arg[1] ? result.length : arg[1]); i++) {
             var get = got(result[i].url, {https: {rejectUnauthorized: false}});
             var stream = get.buffer();
                 
@@ -16,6 +18,6 @@ Asena.addCommand({pattern: 'imgs ?(.*)', fromMe: true, desc: "Search any number 
             });
         }
 
-        message.reply("Uploading photo ```{}``` number ```{} `..."((result.length < arg ? result.length : arg), (match[1].split(':')[0]).split(':')[0]));
+        message.reply("Uploading photo ```{}``` number ```{} `..."((result.length < arg[1] ? result.length : arg[1]), arg[0]));
     });
 }));
