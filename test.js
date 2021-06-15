8b8a59ccd925a8231d351ce71a2a7ea1f1bf8aef
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-15 17:37:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const Asena = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 
 Asena.addCommand({pattern: 'utest ?(.*)', fromMe: true, desc: "fucking test" }, (async (message, match) => { 
-	return await message.client.sendMessage(message.jid,{ url: match[1] },MessageType.file , { mimetype: Mimetype.application/octet-stream, caption: "test bro!" });
+	return await message.client.sendMessage(message.jid,{ url: match[1] },MessageType.file , { mimetype: Mimetype.application, caption: "test bro!" });
 }));
