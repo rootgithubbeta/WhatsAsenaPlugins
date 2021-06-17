bbbb819414d20fb861a38b8748bda30a43bc1ccb
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-17 16:10:08 +0530

    Update imgsearch.js

--- imgsearch.js
+++ imgsearch.js
@@ -5,11 +5,11 @@ const got = require('got');
 Asena.addCommand({pattern: 'gimg ?(.*)', fromMe: true, desc: 'Search any number of pictures' }, (async (message, match) => { 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'What do you what to search : number of images want',MessageType.text);
     message.client.sendMessage(message.jid,match[1], MessageType.text);
+    try {
     var arg = match[1].split(':') ;
     message.client.sendMessage(message.jid,arg[0], MessageType.text);
     message.client.sendMessage(message.jid,arg[1], MessageType.text);
     if (arg[1]=== '') arg[1] = 5 ;
-	try {
 	 gis(arg[0], async (error, result) => {
          for (var i = 0; i < (result.length < arg[1] ? result.length : arg[1]); i++) {
              var get = got(result[i].url, {https: {rejectUnauthorized: false}});
