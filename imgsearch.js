6a1dce24bc86e427e2cc7b5942feceea88a2c3ca
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-17 16:06:33 +0530

    Update imgsearch.js

--- imgsearch.js
+++ imgsearch.js
@@ -6,21 +6,21 @@ Asena.addCommand({pattern: 'gimg ?(.*)', fromMe: true, desc: 'Search any number 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'What do you what to search : number of images want',MessageType.text);
     message.client.sendMessage(message.jid,match[1], MessageType.text);
     var arg = match[1].split(':') ;
-	message.client.sendMessage(message.jid,arg[0], MessageType.text);
-	message.client.sendMessage(message.jid,arg[1], MessageType.text);
-	if (arg[1]=== '') arg[1] = 5 ;
+    message.client.sendMessage(message.jid,arg[0], MessageType.text);
+    message.client.sendMessage(message.jid,arg[1], MessageType.text);
+    if (arg[1]=== '') arg[1] = 5 ;
 	try {
-	gis(arg[0], async (error, result) => {
-        for (var i = 0; i < (result.length < arg[1] ? result.length : arg[1]); i++) {
-            var get = got(result[i].url, {https: {rejectUnauthorized: false}});
-            var stream = get.buffer();
+	 gis(arg[0], async (error, result) => {
+         for (var i = 0; i < (result.length < arg[1] ? result.length : arg[1]); i++) {
+             var get = got(result[i].url, {https: {rejectUnauthorized: false}});
+             var stream = get.buffer();
                 
-            stream.then(async (image) => {
-                await message.client.sendMessage(message.jid,image, MessageType.image);
-            });
-        }
+             stream.then(async (image) => {
+                 await message.client.sendMessage(message.jid,image, MessageType.image);
+             });
+         }
 
-        message.reply('Uploading ${arg[1]} photo(s) of ${arg[0]}');
+         message.reply('Uploading ${arg[1]} photo(s) of ${arg[0]}');
     });
 	} catch (e) {
 		message.client.sendMessage(message.jid,e, MessageType.text);
