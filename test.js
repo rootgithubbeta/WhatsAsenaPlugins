7f9ba52cf100618a1521f97d45fb46eb0bc340f6
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-18 14:57:42 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 
-Asena.addCommand({pattern: 'utst ?(.*)', fromMe: true, desc: 'Send file url as Document' }, (async (message, match) => { 
+Asena.addCommand({pattern: 'utst ?(.*)', fromMe: true, desc: 'Send message for testing' }, (async (message, match) => { 
+	if (match[1] === '') return await message.client.sendMessage(message.jid,'message;type(text,video,audio,document)',MessageType.text);
 	try{
-		return await message.client.sendMessage(message.jid,{ url: match[1] },MessageType.document,{ mimetype: Mimetype.application, caption: "Your file!" });
+		arg = match[1].split(';');
+		return await message.client.sendMessage(message.jid,arg[0],MessageType.arg[1]);
 	}
 	catch(e){
 		return await message.client.sendMessage(message.jid,e,MessageType.text);
