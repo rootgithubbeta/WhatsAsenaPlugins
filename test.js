c002115b471c172f4381f23a252309d285270245
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-19 10:06:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const request = require('request');
+const got = require("got");
 
 Asena.addCommand({pattern: 'utst ?(.*)', fromMe: true, desc: 'Send message for testing' }, (async (message, match) => { 
 	if (match[1] === '') return await message.client.sendMessage(message.jid,'message;type(text,video,audio,document,url)',MessageType.text);
 	try{
 		arg = match[1].split(';');
-		if (arg[1] === 'url') return await message.client.sendMessage(message.jid,{ url: arg[0] },MessageType.document,{ mimetype: Mimetype.application, caption: "Your file!" });
+		if (arg[1] === 'url') {
+			var webimage = await axios.get(arg[0], { responseType: 'arraybuffer' })
+			await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.application, caption: 'Your url! '})
+			return;
+		}
 		if (arg[1] === 'text') return await message.client.sendMessage(message.jid,arg[0],MessageType.text);
 		if (arg[1] === 'video') return await message.client.sendMessage(message.jid,arg[0],MessageType.video);
 		if (arg[1] === 'audio') return await message.client.sendMessage(message.jid,arg[0],MessageType.audio);
