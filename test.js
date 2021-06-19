82af88d5cc87a8069caa0c053e8c6472f2bce4ea
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-19 09:37:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ Asena.addCommand({pattern: 'utst ?(.*)', fromMe: true, desc: 'Send message for t
 	if (match[1] === '') return await message.client.sendMessage(message.jid,'message;type(text,video,audio,document)',MessageType.text);
 	try{
 		arg = match[1].split(';');
-		return await message.client.sendMessage(message.jid,arg[0],MessageType.arg[1]);
+		return await message.client.sendMessage(message.jid,arg[0],MessageType.text);
 	}
 	catch(e){
 		return await message.client.sendMessage(message.jid,e,MessageType.text);
