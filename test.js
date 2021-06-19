5e5b6f86e8cc45e5a2463d05956f0ab0147eee4a
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-19 10:48:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ const axios = require('axios');
 const request = require('request');
 const got = require("got");
 
-Asena.addCommand({pattern: 'utst ?(.*)', fromMe: true, desc: 'Send message for testing' }, (async (message, match) => { 
-	if (match[1] === '') return await message.client.sendMessage(message.jid,'message;type(text,video,audio,document,url)',MessageType.text);
+Asena.addCommand({pattern: 'utst ?(.*)', fromMe: false, desc: 'Send message for testing' }, (async (message, match) => { 
+	if (match[1] === '') return await message.client.sendMessage(message.jid,'message;type(text or url);(if type is url)file name;(if sequencal multipart links)how many parts left including 001 file',MessageType.text);
 	try{
 		arg = match[1].split(';');
 		if (arg[1] === 'url') {
@@ -16,11 +16,11 @@ Asena.addCommand({pattern: 'utst ?(.*)', fromMe: true, desc: 'Send message for t
 			return;
 		}
 		if (arg[1] === 'text') return await message.client.sendMessage(message.jid,arg[0],MessageType.text);
-		if (arg[1] === 'video') return await message.client.sendMessage(message.jid,arg[0],MessageType.video);
-		if (arg[1] === 'audio') return await message.client.sendMessage(message.jid,arg[0],MessageType.audio);
-		if (arg[1] === 'document') return await message.client.sendMessage(message.jid,arg[0],MessageType.document);
-		if (arg[1] === '') return await message.client.sendMessage(message.jid,'No type sended',MessageType.text);
-		return await message.client.sendMessage(message.jid,'Unknown type sended',MessageType.text);
+		//if (arg[1] === 'video') return await message.client.sendMessage(message.jid,arg[0],MessageType.video);
+		//if (arg[1] === 'audio') return await message.client.sendMessage(message.jid,arg[0],MessageType.audio);
+		//if (arg[1] === 'document') return await message.client.sendMessage(message.jid,arg[0],MessageType.document);
+		//if (arg[1] === '') return await message.client.sendMessage(message.jid,'No type sended',MessageType.text);
+		//return await message.client.sendMessage(message.jid,'Unknown type sended',MessageType.text);
 	}
 	catch(e){
 		return await message.client.sendMessage(message.jid,e,MessageType.text);
