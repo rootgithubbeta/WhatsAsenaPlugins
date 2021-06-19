c5d835ab8155685b05fe0c375c622aa4313e2530
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-19 10:52:16 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const request = require('request');
 const got = require("got");
 
-Asena.addCommand({pattern: 'utst ?(.*)', fromMe: false, desc: 'Send message for testing' }, (async (message, match) => { 
+Asena.addCommand({pattern: 'wget ?(.*)', fromMe: false, desc: 'Send message for testing' }, (async (message, match) => { 
 	if (match[1] === '') return await message.client.sendMessage(message.jid,'message;type(text or url);(if type is url)file name;(if sequencal multipart links)how many parts left including 001 file',MessageType.text);
 	try{
 		arg = match[1].split(';');
@@ -13,7 +13,7 @@ Asena.addCommand({pattern: 'utst ?(.*)', fromMe: false, desc: 'Send message for 
 			var webimage = await axios.get(arg[0], { responseType: 'arraybuffer' })
 			await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: 'application/octet-stream' ,filename:arg[2]})
 			//await message.sendMessage(Buffer.from(webimage.data), MessageType.video)
-			return;
+			return await message.client.sendMessage(message.jid,'Your file is in WhatsAppDocuments folder',MessageType.text);
 		}
 		if (arg[1] === 'text') return await message.client.sendMessage(message.jid,arg[0],MessageType.text);
 		//if (arg[1] === 'video') return await message.client.sendMessage(message.jid,arg[0],MessageType.video);
