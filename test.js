520bdbdcf1a583f507b4bc64b82b3d22a7ac2320
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-19 11:40:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,28 +5,39 @@ const axios = require('axios');
 const request = require('request');
 const got = require("got");
 
-Asena.addCommand({pattern: 'fetch ?(.*)', fromMe: false, desc: 'Send text message or Remote url' }, (async (message, match) => { 
+Asena.addCommand({pattern: 'wget ?(.*)', fromMe: false, desc: 'Send text message or Remote url' }, (async (message, match) => { 
 	if (match[1] === '') {
 		await message.client.sendMessage(message.jid,'message;type(text or url);(if type is url)file name;(if sequencal multipart links)how many parts left including 001 file',MessageType.text);
 		await message.client.sendMessage(message.jid,'*Examples*',MessageType.text);
-		await message.client.sendMessage(message.jid,'*Text msg* - .fetch hi this is text msg;text',MessageType.text);
-		await message.client.sendMessage(message.jid,'*Normal Direct link* - .fetch https://example.com/example.pdf;url;example.pdf',MessageType.text);
-		return await message.client.sendMessage(message.jid,'*Multi Part split url* (assume it has 5 parts) - .fetch https://example.com/example.pdf.001;url;example.pdf;5',MessageType.text);
+		await message.client.sendMessage(message.jid,'*Text msg* - .wget hi this is text msg;text',MessageType.text);
+		await message.client.sendMessage(message.jid,'*Normal Direct link* - .wget https://example.com/example.pdf;url;example.pdf',MessageType.text);
+		return await message.client.sendMessage(message.jid,'*Multi Part split url* (assume it has 5 parts) - .wget https://example.com/example.pdf(without .001);url;example.pdf;5',MessageType.text);
 	}
 	try{
 		arg = match[1].split(';');
-		if (arg[1] === 'url') {
-			var webimage = await axios.get(arg[0], { responseType: 'arraybuffer' })
-			await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: 'application/octet-stream' ,filename:arg[2]})
-			//await message.sendMessage(Buffer.from(webimage.data), MessageType.video)
-			return await message.client.sendMessage(message.jid,'Your file is in WhatsAppDocuments folder',MessageType.text);
-		}
-		if (arg[1] === 'text') return await message.client.sendMessage(message.jid,arg[0],MessageType.text);
-		//if (arg[1] === 'video') return await message.client.sendMessage(message.jid,arg[0],MessageType.video);
-		//if (arg[1] === 'audio') return await message.client.sendMessage(message.jid,arg[0],MessageType.audio);
-		//if (arg[1] === 'document') return await message.client.sendMessage(message.jid,arg[0],MessageType.document);
-		//if (arg[1] === '') return await message.client.sendMessage(message.jid,'No type sended',MessageType.text);
-		//return await message.client.sendMessage(message.jid,'Unknown type sended',MessageType.text);
+		if (arg[3] == ''){
+			
+			if (arg[1] === 'url') {
+				var webimage = await axios.get(arg[0], { responseType: 'arraybuffer' });
+				await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: 'application/octet-stream' ,filename:arg[2]});
+				//await message.sendMessage(Buffer.from(webimage.data), MessageType.video)
+				return await message.client.sendMessage(message.jid,'Your file is in WhatsAppDocuments folder',MessageType.text);
+			}
+			if (arg[1] === 'text') return await message.client.sendMessage(message.jid,arg[0],MessageType.text);
+			//if (arg[1] === 'video') return await message.client.sendMessage(message.jid,arg[0],MessageType.video);
+			//if (arg[1] === 'audio') return await message.client.sendMessage(message.jid,arg[0],MessageType.audio);
+			//if (arg[1] === 'document') return await message.client.sendMessage(message.jid,arg[0],MessageType.document);
+			//if (arg[1] === '') return await message.client.sendMessage(message.jid,'No type sended',MessageType.text);
+			//return await message.client.sendMessage(message.jid,'Unknown type sended',MessageType.text);
+		} else {
+			for (let i = 1; i <= parseInt(arg[3]); i++) {
+				var str = "" + arg[3] ;
+				var pad = "000" ;
+				var ans = pad.substring(0, pad.length - str.length) + str ;
+				await message.client.sendMessage(message.jid,arg[0]+ans,MessageType.text);
+			}
+			return;
+	}
 	}
 	catch(e){
 		return await message.client.sendMessage(message.jid,e,MessageType.text);
