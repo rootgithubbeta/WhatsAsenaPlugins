5f7fc5fa7eccc2a5609776c42de89ffddee5ad50
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-22 20:52:22 +0530

    Add files via upload

---
+++ m4a2mp3.js
@@ -0,0 +1,33 @@
+const Asena = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+
+
+    Asena.addCommand({pattern: 'm4a2mp3$', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,"reply with a m4a", MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,'Downloadin Audio',MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+
+        ffmpeg(location)
+            .withNoVideo()
+			.inputFormat('m4a')
+			.audioCodec('libmp3lame')
+			.audioBitrate(128)
+			.format('mp3')
+			//.on('error', (err) => console.error(err))
+			//.on('end', () => console.log('Finished!'))
+            .save('new.mp3')
+            .on('end', async () => {
+                await message.sendMessage(fs.readFileSync('new.mp3'), MessageType.audio);
+            });
+        return;
+    }));
