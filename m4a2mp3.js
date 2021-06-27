37f828614419b74e6ef97822dd791cb7fd370007
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-27 19:48:08 +0530

    Update m4a2mp3.js

--- m4a2mp3.js
+++ m4a2mp3.js
@@ -19,13 +19,14 @@ const ffmpeg = require('fluent-ffmpeg');
 
         ffmpeg(location)
 		.withNoVideo()
-		.inputFormat('mp4')
+		//.inputFormat('m4a')
 		.audioCodec('libmp3lame')
 		.audioBitrate(128)
-		.format('mp3')
+		//.format('mp3')
 		//.on('error', (err) => console.error(err))
 		//.on('end', () => console.log('Finished!'))
-		.save(fs.createWriteStream("new.mp3"))
+		//.save(fs.createWriteStream("new.mp3"))
+	    	.save('new.mp3')    
 		.on('error', async (err) => {await message.sendMessage(`error ${err}`, MessageType.text);})
 		.on('end', async () => { await message.sendMessage(fs.readFileSync('new.mp3'), MessageType.audio);}); 
 		return;
