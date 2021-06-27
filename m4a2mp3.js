a1d169a7d36c690365056515c087933d15944918
rootgithubbeta <71202701+rootgithubbeta@users.noreply.github.com>
2021-06-27 20:02:07 +0530

    Update m4a2mp3.js

--- m4a2mp3.js
+++ m4a2mp3.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 
 
-    Asena.addCommand({pattern: 'm4a2mp3$', fromMe: false, desc: 'Convert m4a to mp3'}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'm4a2mp3 ?(.*)', fromMe: false, desc: 'Convert m4a to mp3'}, (async (message, match) => {    
 
-        if (message.reply_message === false) return await message.client.sendMessage(message.jid,'reply with a m4a', MessageType.text);
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,'reply with a m4a and a file name', MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,'Downloadin Audio',MessageType.text);
         var location = await message.client.downloadAndSaveMediaMessage({
             key: {
@@ -28,6 +28,6 @@ const ffmpeg = require('fluent-ffmpeg');
 		//.save(fs.createWriteStream("new.mp3"))
 	    	.save('new.mp3')    
 		.on('error', async (err) => {await message.sendMessage(`error ${err}`, MessageType.text);})
-		.on('end', async () => { await message.sendMessage(fs.readFileSync('new.mp3'), MessageType.document, {mimetype: 'audio/mpeg', ptt: false});}); 
+		.on('end', async () => { await message.sendMessage(fs.readFileSync('new.mp3'), MessageType.document, {mimetype: 'audio/mpeg',filename: match[1], ptt: false});}); 
 		return;
     }));
